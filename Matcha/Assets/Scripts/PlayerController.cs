e0f57fa9230262d325e0722d97b23859cff2a69d
Austin Szema <aszema13@gmail.com>
2022-07-03 22:04:53 -0400

    Reworked input system
    Changed the script for handling input
    
    Mouse position detection working
    
    Need to add shooting

--- Matcha/Assets/Scripts/PlayerController.cs
+++ Matcha/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,83 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
     public GameObject groundCheck;
     public LayerMask groundLayer;
+    public Camera mainCam;
 
-    private float horizontal;
-
+    [SerializeField] private GameObject bulletPrefab;
+    
     private bool isFacingRight = true;
     private bool isGrounded = false;
 
     private Vector2 lastVelocity = Vector2.zero;
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 mousePosition = Vector2.zero;
 
     //controls how fast player moves
-    [SerializeField]
-    private float speed;
+    [SerializeField] private float speed;
 
     //controls how high player jumps
-    [SerializeField]
-    private float jumpHeight;
+    [SerializeField] private float jumpHeight;
 
     //controls how much force is added downwards when the player is falling
-    [SerializeField]
-    private float downGravity;
+    [SerializeField] private float downGravity;
+
+
+    public PlayerInputActions playerControls;
+    private InputAction move;
+    private InputAction fire;
+    private InputAction look;
+    private InputAction jump;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+
+
+        look = playerControls.Player.Look;
+        look.Enable();
+        look.performed += Look;
+
+        jump = playerControls.Player.Jump;
+        jump.Enable();
+        jump.performed += Jump;
+        
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+        look.Disable();
+        jump.Disable();
+    }
 
     //Update is called once every frame
     void Update()
     {
+        moveDirection = move.ReadValue<Vector2>();
+
         //if the ground check circle overlaps with the ground, the player is grounded (and can jump again)
         if (Physics2D.OverlapCircle(groundCheck.transform.position, groundCheck.transform.localScale.y, groundLayer))
         {
@@ -40,11 +89,11 @@ public class PlayerController : MonoBehaviour
         }
 
         //flips player 
-        if (!isFacingRight && horizontal > 0f)
+        if (!isFacingRight && moveDirection.x > 0f)
         {
             Flip();
         }
-        else if (isFacingRight && horizontal < 0f)
+        else if (isFacingRight && moveDirection.x < 0f)
         {
             Flip();
         }
@@ -52,18 +101,18 @@ public class PlayerController : MonoBehaviour
 
 
 
-    //fixed update is also called once every frame but is frame rate independent (good for handling input)
+    //fixed update is also called once every frame but is frame rate independent (good for physics (rigidbody stuff))
     private void FixedUpdate()
     {
 
         //fixes horizontal drift
-        if (horizontal > -0.3f && horizontal < 0.3f)
+        if (moveDirection.x > -0.3f && moveDirection.x < 0.3f)
         {
-            horizontal = 0f;
+            moveDirection.x = 0f;
         }
 
         //this is the line of code that actually moves the player
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
 
 
         //if your current y velocity is less than your y velocity on the last frame (falling), add gravity
@@ -87,8 +136,37 @@ public class PlayerController : MonoBehaviour
         transform.localScale = localScale;
     }
 
+    private void Fire(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            GameObject bullet;
+            bullet = Instantiate(bulletPrefab, mousePosition, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().AddForce(mousePosition);
+        }
+    }
+
+    private void Look(InputAction.CallbackContext context)
+    {
+        mousePosition = playerControls.Player.Look.ReadValue<Vector2>();
+        mousePosition = mainCam.ScreenToWorldPoint(mousePosition);
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+        if (context.canceled && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+    }
 
 
+    /*
     //handles horizontal player movement 
     public void Move(InputAction.CallbackContext context)
     {
@@ -116,6 +194,13 @@ public class PlayerController : MonoBehaviour
         Debug.Log("fired");
     }
 
+    public void Look(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
 
+    }
+
+    */
 
 }
